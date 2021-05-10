aa1b446736986d58ff8f5331e7a951fc2834a63b
Danny Calise <dannyjcalise@gmail.com>
2021-05-10 11:28:31 -0400

    bootstrap installed

---
+++ Desktop/repos/openapi-vue/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Whaddup</h1>
+  </div>
+</template>
+
+<script>
+import OpenAPIClientAxios from "openapi-client-axios";
+
+async () => {
+  const api = new OpenAPIClientAxios({
+    definition:
+      "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore.yaml",
+  });
+  const client = await api.init();
+  const res = await api.getPets(); // res.data will contain the result of the API call
+  console.log(res);
+  console.log(client);
+
+};
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
